5008eeec51b25d31d97c101256b6fdb34a628e5f
noevasquezc-bit <noe.vasquezc@gmail.com>
2026-06-05 17:43:03 -0400

    fix: replace stripe_api with paypal_api

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,5 +1,5 @@
 from .airtable_api import create_lead, update_lead_status, get_lead_by_whatsapp, create_reserva, confirm_payment, log_daily_metrics, get_pilot_totals
 from .whatsapp_api import send_text, send_quote, SCRIPTS, parse_webhook_event
 from .meta_api import evaluate_and_enforce_cpa, pause_campaign, get_active_campaigns
-from .stripe_api import create_payment_link, verify_payment, handle_webhook
+from .paypal_api import create_payment_order, capture_payment, handle_webhook, get_payment_link
 from .google_maps import estimate_price, get_directions_url
