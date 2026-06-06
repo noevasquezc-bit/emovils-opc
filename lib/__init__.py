b6c1a0e87108eb2275fabe505c674b816c3bfd25
Emovils <noe.vasquezc@gmail.com>
2026-06-06 08:03:50 +0000

    fix: reemplazar estimate_price por calculate_route_price en lib/__init__.py

--- lib/__init__.py
+++ lib/__init__.py
@@ -2,4 +2,4 @@ from .airtable_api import create_lead, update_lead_status, get_lead_by_whatsapp,
 from .whatsapp_api import send_text, send_quote, SCRIPTS, parse_webhook_event
 from .meta_api import evaluate_and_enforce_cpa, pause_campaign, get_active_campaigns
 from .paypal_api import create_payment_order, capture_payment, handle_webhook, get_payment_link
-from .google_maps import estimate_price, get_directions_url
+from .google_maps import calculate_route_price, get_directions_url
